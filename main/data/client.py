cd8ecfe2a87096ada8bbfd4538b2e893df8b07a3
Tong Zhang <zhangt@frib.msu.edu>
2021-06-03 17:03:26 -0400

    ENH: Return pandas.DataFrame from .list and .contents methods.

--- main/data/client.py
+++ main/data/client.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import pandas as pd
 import requests
 from simplejson import JSONDecodeError
 
@@ -35,6 +36,8 @@ class SpecTclDataClient(object):
         self._port = port
         self._group = group
         self.update_base_uri()
+        #
+        self._vlist_cache = self.list()
 
     @property
     def name(self):
@@ -104,6 +107,59 @@ class SpecTclDataClient(object):
         r = requests.get(url, params=action_params, verify=False)
         return make_response(r)
 
+    def list(self, **kws):
+        """List defined spectra.
+
+        Keyword Arguments
+        -----------------
+        filter : str
+            Unix wildcard pattern as the name filter.
+        update_cache : bool
+            If set, update the cached value for spectrum configurations.
+
+        Returns
+        -------
+        r : DataFrame
+            Table of spectrum configurations.
+        """
+        r = self.get("list", **kws)
+        df = pd.DataFrame(columns=['Name', 'Type', 'Parameters', 'Axes', 'ChanType'])
+        for i, v in enumerate(r):
+            df.loc[i] = v['name'], v['type'], v['parameters'], v['axes'], v['chantype']
+        df.set_index('Name', inplace=True)
+        if kws.get('update_cache', False):
+            self._vlist_cache = df
+        return df
+
+    def contents(self, name, **kws):
+        """Return the data of spectrum named *name*.
+
+        Parameters
+        ----------
+        name : str
+            Name of the spectrum.
+
+        Keyword Arguments
+        -----------------
+        refresh_cache : bool
+            If set, refresh the cache of spectra info.
+        """
+        try:
+            data = self.get("contents", name=name, **kws)
+        except JSONDecodeError:
+            return None
+        else:
+            df = pd.DataFrame.from_dict(data['channels'])
+            if kws.get('refresh_cache', False):
+                self.list()
+            spec_conf = self._vlist_cache.loc[name]
+            params = spec_conf.Parameters
+            if len(params) == 1: # type '1'
+                df.rename(columns={'x': params[0], 'v': 'count'}, inplace=True)
+            elif len(params) == 2: # type '2'
+                df.rename(columns={'x': params[0], 'y': params[1], 'v': 'count'}, inplace=True)
+            return df
+
     def validate_action(self, action, action_params):
         params = ACTION_PARAMS[self._group][action]
         for i in action_params:
@@ -116,7 +172,6 @@ class SpecTclDataClient(object):
 
     def __repr__(self):
         return "[Data Client] SpecTcl REST Service on: {}:{}/spectcl".format(self._base_url, self._port)
-
 
 
 if __name__ == '__main__':
