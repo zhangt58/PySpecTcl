9c4a873da9cf11548052c8195a7b6f9d06d129f9
Tong Zhang <zhangt@frib.msu.edu>
2021-06-04 13:10:46 -0400

    MNT: Update package info.

--- main/__init__.py
+++ main/__init__.py
@@ -2,5 +2,8 @@ from spectcl.client import *
 
 __version__ = '0.0.3'
 __author__ = 'Tong Zhang <zhangt@frib.msu.edu>'
-__doc__ ="""spectcl: Python interface to SpecTcl REST server."""
+__name__ = "PySpecTcl"
+__doc__ ="""Python interface to SpecTcl REST server."""
 
+def info():
+    print(f"{__name__} (v{__version__}): {__doc__}\nDeveloped by: {__author__}")
