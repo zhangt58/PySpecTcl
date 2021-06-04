30d8a11600556f789e735fbf14f323769b8d8ad8
Tong Zhang <zhangt@frib.msu.edu>
2021-06-04 13:23:15 -0400

    MNT: Bump version to 0.0.4.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extra_require = {
 
 setup(
         name='pyspectcl',
-        version='0.0.3',
+        version='0.0.4',
         description='Python interface to SpecTcl server',
         author='Tong Zhang',
         author_email='zhangt@frib.msu.edu',
