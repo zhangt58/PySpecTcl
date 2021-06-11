2ace7c7e138830eb77387462c89206456b843e72
Tong Zhang <zhangt@frib.msu.edu>
2021-06-11 11:43:08 -0400

    MNT: Bump up version to 0.2.0.

--- main/__init__.py
+++ main/__init__.py
@@ -1,7 +1,7 @@
 from spectcl.client import *
 from spectcl.data import Spectrum
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Tong Zhang <zhangt@frib.msu.edu>'
 __name__ = "PySpecTcl"
 __doc__ ="""Python interface to SpecTcl REST server."""
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def set_entry_points():
 
 setup(
     name='pyspectcl',
-    version='0.1.0',
+    version='0.2.0',
     description='Python interface to SpecTcl server',
     author='Tong Zhang',
     author_email='zhangt@frib.msu.edu',
