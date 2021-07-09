76f7ea28bdc81ac0105da2e6c306d74b5b676aec
Tong Zhang <zhangt@frib.msu.edu>
2021-07-09 16:40:40 -0400

    MNT: Add GateApplyClient into spectcl.client namespace.
    - alias of spectcl.data.SpecTclApplyClient

--- main/client/__init__.py
+++ main/client/__init__.py
@@ -1,2 +1,3 @@
 from spectcl.data import SpecTclDataClient as DataClient
 from spectcl.data import SpecTclGateClient as GateClient
+from spectcl.data import SpecTclApplyClient as GateApplyClient
--- main/data/__init__.py
+++ main/data/__init__.py
@@ -1,3 +1,4 @@
 from .client import SpecTclDataClient
 from .client import SpecTclGateClient
+from .client import SpecTclApplyClient
 from .utils import Spectrum
