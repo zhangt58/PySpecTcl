6c619df02834180791ac9c20cf2c1ecf40715e6a
Tong Zhang <zhangt@frib.msu.edu>
2021-07-14 13:44:49 -0400

    MNT: Bump version to 0.3.0.

--- main/__init__.py
+++ main/__init__.py
@@ -2,10 +2,10 @@ from spectcl.client import *
 from spectcl.contrib import *
 from spectcl.data import Spectrum
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Tong Zhang <zhangt@frib.msu.edu>'
 __name__ = "PySpecTcl"
 __doc__ ="""Python interface to SpecTcl REST server."""
 
 def info():
-    print(f"{__name__} (v{__version__}): {__doc__}\nDeveloped by: {__author__}")
+    print(f"{__name__} (v{__version__}): {__doc__}\nContact: {__author__}")
