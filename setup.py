3700e86d8c5eab2864acd40c1ae6025909002958
Tong Zhang <zhangt@frib.msu.edu>
2022-04-26 15:25:10 -0400

    MNT: Bump version to 0.3.1.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def set_entry_points():
 
 setup(
     name='pyspectcl',
-    version='0.3.0',
+    version='0.3.1',
     description='Python interface to SpecTcl server',
     author='Tong Zhang',
     author_email='zhangt@frib.msu.edu',
